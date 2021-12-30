526df74a5a46e33e5e2a43baffbee3d186993e0e
Claudia <pinguiri1987@gmail.com>
2021-12-29 21:54:18 -0500

    adding galeria contacto and servicios pages

---
+++ pages/contacto.js
@@ -0,0 +1,10 @@
+import Layout from "../Components/Layout"
+
+const Contacto = ()=>{
+   return (
+     <Layout title={"contacto"}>
+       <h1>Contacto</h1>
+     </Layout>
+   );
+}
+export default Contacto
---
+++ pages/servicios.js
@@ -0,0 +1,10 @@
+import Layout from "../Components/Layout"
+
+const Servicios = ()=>{
+   return (
+     <Layout title={"servicios"}>
+       <h1>servicios</h1>
+     </Layout>
+   );
+}
+export default Servicios

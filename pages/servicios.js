e4b63ca238bfe21826b0a5d1895f9dbac514a046
Carlos Aguirre Z <caaz160782@gmail.com>
2022-01-17 22:32:49 -0600

    landig

--- pages/servicios.js
+++ pages/servicios.js
@@ -13,64 +13,76 @@ import {
   CardActions,
   CardContent,
   CardMedia,
-  shadows
+  shadows,
 } from "@mui/material";
-import useStyles from './servicios/style.js';
-import Image from 'next/image'
+import useStyles from "./servicios/style.js";
+import Image from "next/image";
 import theme from "../utils/temaConfig";
 // import backimg from "../public/images/hand.png";
 
-
-const Servicios = ()=>{
-  const classes = useStyles() 
+const Servicios = () => {
+  const classes = useStyles();
   return (
-     <Layout>
-       <div>
-          <Box className={classes.title}>
-            <Typography variant="h2" className={classes.blogsTitle}>
-              Nuestros servicios
-            </Typography>
-            {/* <Image 
+    <div>
+      <Box className={classes.title}>
+        <Typography variant="h2" className={classes.blogsTitle}>
+          Nuestros servicios
+        </Typography>
+        {/* <Image 
               src="/hand.png" 
               width={500} 
               height={500}
             /> */}
-          </Box>
-          <Container maxWidth="lg" className={classes.blogsContainer}>
-            <Grid 
-                container
-                direction="colunm"
-                justifyContent="center"
-                alignItems="center"
-                spacing={1}
-            >
-              <Grid item sm={12} md={6}>
-                <Image 
-                  src="/images/hand.png"
-                  height="300px"
-                  width="400px"
-                  alt="hand"
-                />
-              </Grid>
-              <Grid item sm={12} md={6}>
-                <Typography variant="body1">
-                  Perfect time Ink es una excelente herramienta de control de agenda, ya que todo se presenta de manera virtual, y es fácil de acceder a sus servicios, a través de cualquier dispositivo, brindando la opción a los dueños de estudios de tatuaje el control de su estudio al alcance de sus manos, 
-                  Del lado de los tatuadores, les ayuda a llevar un mejor control de su tiempo mediante la visualización de su agenda completa, por mes, día o semana. 
-                  Para los clientes de los estudios, les brinda la facilidad de agendar su cita de una manera más visual, mostrando los espacios disponibles para agendar su cita por cada tatuador, la plataforma les dará la opción de conocer el trabajo de los tatuadores mediante sus redes sociales, y darles recordatorios de sus citas activas.
-                </Typography>
-              </Grid>
-            </Grid>
-            <Box className={classes.boxcont}>
-            <Typography variant="body2" className={classes.title}>
-              Da click aqui si deseas crear un perfil con nosotros
+      </Box>
+      <Container maxWidth="lg" className={classes.blogsContainer}>
+        <Grid
+          container
+          direction="colunm"
+          justifyContent="center"
+          alignItems="center"
+          spacing={1}
+        >
+          <Grid item sm={12} md={6}>
+            <Image
+              src="/images/hand.png"
+              height="300px"
+              width="400px"
+              alt="hand"
+            />
+          </Grid>
+          <Grid item sm={12} md={6}>
+            <Typography variant="body1">
+              Perfect time Ink es una excelente herramienta de control de
+              agenda, ya que todo se presenta de manera virtual, y es fácil de
+              acceder a sus servicios, a través de cualquier dispositivo,
+              brindando la opción a los dueños de estudios de tatuaje el control
+              de su estudio al alcance de sus manos, Del lado de los tatuadores,
+              les ayuda a llevar un mejor control de su tiempo mediante la
+              visualización de su agenda completa, por mes, día o semana. Para
+              los clientes de los estudios, les brinda la facilidad de agendar
+              su cita de una manera más visual, mostrando los espacios
+              disponibles para agendar su cita por cada tatuador, la plataforma
+              les dará la opción de conocer el trabajo de los tatuadores
+              mediante sus redes sociales, y darles recordatorios de sus citas
+              activas.
             </Typography>
-            <Button className={classes.btn} variant="contained" color="primary" href="/servicios">
-              Log in
-            </Button>
-            </Box>
-          </Container>
-       </div>  
-     </Layout>
-   );
-}
-export default Servicios
+          </Grid>
+        </Grid>
+        <Box className={classes.boxcont}>
+          <Typography variant="body2" className={classes.title}>
+            Da click aqui si deseas crear un perfil con nosotros
+          </Typography>
+          <Button
+            className={classes.btn}
+            variant="contained"
+            color="primary"
+            href="/servicios"
+          >
+            Log in
+          </Button>
+        </Box>
+      </Container>
+    </div>
+  );
+};
+export default Servicios;
